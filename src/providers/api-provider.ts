f64e48e9b31f0db07afa11b0c752282ec7ebef78
Eliel Gomes Hyertquist <elielgomespg@hotmail.com>
2024-02-29 22:34:58 -0300

    feat: Implementado axios para os providers

--- src/providers/api-provider.ts
+++ src/providers/api-provider.ts
@@ -1,53 +1,59 @@
+import axios from "axios";
 import { IGenreList, IMovie, IMovieList, ITrailer } from "../interfaces/";
-
-const apiKey = process.env.VITE_API_KEY;
-const baseUrlGenres = process.env.VITE_BASE_URL_GENRES;
-const baseUrlMovie = process.env.VITE_BASE_URL_MOVIE;
+const apiKey = process.env.REACT_APP_API_KEY;
 
 export const GetTrailer = async (id: string) => {
-	const response = await fetch(`${baseUrlMovie}${id}/videos?${apiKey}&language=pt-br`);
-	const data: ITrailer = await response.json();
+	const response = await axios.get(
+		`https://api.themoviedb.org/3/movie/${id}/videos?api_key=${apiKey}&language=pt-br`,
+	);
+	const data: ITrailer = await response.data;
 	return data;
 };
 
 export const GetGenreList = async () => {
-	const response = await fetch(`${baseUrlGenres}movie/list?${apiKey}&language=pt-br`);
-	const data: IGenreList = await response.json();
+	const response = await axios.get(
+		`https://api.themoviedb.org/3/genre/movie/list?api_key=${apiKey}&language=pt-br`,
+	);
+	const data: IGenreList = await response.data;
 	return data;
 };
 
 export const GetSearchMovies = async (query: string, page: number) => {
-	const response = await fetch(
-		`https://api.themoviedb.org/3/search/movie?${apiKey}&query=${query}&include_adult=false&language=pt-br&page=${page}`,
+	const response = await axios.get(
+		`https://api.themoviedb.org/3/search/movie?api_key=${apiKey}&query=${query}&include_adult=false&language=pt-br&page=${page}`,
 	);
-	const data: IMovieList = await response.json();
+	const data: IMovieList = await response.data;
 	return data;
 };
 
 export const GetMovie = async (id: string) => {
-	const response = await fetch(`${baseUrlMovie}${id}?${apiKey}&language=pt-br`);
-	const data: IMovie = await response.json();
+	const response = await axios.get(
+		`https://api.themoviedb.org/3/movie/${id}?api_key=${apiKey}&language=pt-br`,
+	);
+	const data: IMovie = await response.data;
 	return data;
 };
 
 export const GetPopularMovieList = async (page: number) => {
-	const response = await fetch(`${baseUrlMovie}popular?${apiKey}&language=pt-br&page=${page}`);
-	const data: IMovieList = await response.json();
+	const response = await axios.get(
+		`https://api.themoviedb.org/3/movie/popular?api_key=${apiKey}&language=pt-br&page=${page}`,
+	);
+	const data: IMovieList = await response.data;
 	return data;
 };
 
 export const GetTopMovieList = async (page: number) => {
-	const response = await fetch(
-		`${baseUrlMovie}top_rated?${apiKey}&language=pt-br&page=${page}`,
+	const response = await axios.get(
+		`https://api.themoviedb.org/3/movie/top_rated?api_key=${apiKey}&language=pt-br&page=${page}`,
 	);
-	const data: IMovieList = await response.json();
+	const data: IMovieList = await response.data;
 	return data;
 };
 
 export const GetUpcomingMovieList = async (page: number) => {
-	const response = await fetch(
-		`${baseUrlMovie}upcoming?${apiKey}&language=pt-br&page=${page}`,
+	const response = await axios.get(
+		`https://api.themoviedb.org/3/movie/upcoming?api_key=${apiKey}&language=pt-br&page=${page}`,
 	);
-	const data: IMovieList = await response.json();
+	const data: IMovieList = await response.data;
 	return data;
 };
