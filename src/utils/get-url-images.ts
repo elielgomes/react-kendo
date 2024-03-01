4d3e756aeea6e290f578cec5d42688eb800d334c
Eliel Gomes Hyertquist <elielgomespg@hotmail.com>
2024-03-01 09:17:15 -0300

    feat: Criado funcao para compor url das imagens da api

---
+++ src/utils/get-url-images.ts
@@ -0,0 +1,3 @@
+export const getUrlPosterImage = (path: string) => `https://image.tmdb.org/t/p/w500${path}`;
+
+export const getUrlBackdropImage = (path: string) => `https://image.tmdb.org/t/p/w1280${path}`;
