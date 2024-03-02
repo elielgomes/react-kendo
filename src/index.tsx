b038bcb536deb913ee40f93e62e5d028ee19110e
Eliel Gomes Hyertquist <elielgomespg@hotmail.com>
2024-03-02 15:03:13 -0300

    feat: Adicionado provider do context search input

--- src/index.tsx
+++ src/index.tsx
@@ -1,13 +1,16 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./app/App";
+import { SearchInputProvider } from "./contexts/search-input.context";
 import "./index.scss";
 import reportWebVitals from "./reportWebVitals";
 
 const root = ReactDOM.createRoot(document.getElementById("root") as HTMLElement);
 root.render(
 	<React.StrictMode>
-		<App />
+		<SearchInputProvider>
+			<App />
+		</SearchInputProvider>
 	</React.StrictMode>,
 );
 
