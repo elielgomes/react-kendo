c0c0cbdbfd17117243cccaed1f921e2d7c175e5a
Eliel Gomes Hyertquist <elielgomespg@hotmail.com>
2024-03-02 15:04:55 -0300

    feat: Adicionado rota para pagina de detalhes

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,12 +1,13 @@
 import React from "react";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 
-import { Home, NotFound } from "../pages";
+import { Details, Home, NotFound } from "../pages";
 
 const App: React.FC = () => (
 	<BrowserRouter>
 		<Routes>
 			<Route path="/" element={<Home />} />
+			<Route path="/details/:id" element={<Details />} />
 			<Route path="*" element={<NotFound />} />
 		</Routes>
 	</BrowserRouter>
