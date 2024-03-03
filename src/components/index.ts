f18566113c867014a36ddd74481e5d138646e17b
Eliel Gomes Hyertquist <elielgomespg@hotmail.com>
2024-03-02 23:02:54 -0300

    Export centralizado de componentes

--- src/components/index.ts
+++ src/components/index.ts
@@ -1,4 +1,8 @@
+import { Banner } from "./Banner";
+import { FloatButtonHome } from "./FloatButtonHome";
+import { Loading } from "./Loading";
 import { MovieCard } from "./MovieCard";
 import { StarRating } from "./StarRating";
+import { MovieDetails } from "./MovieDetails";
 
-export { MovieCard, StarRating };
+export { Banner, FloatButtonHome, Loading, MovieCard, StarRating, MovieDetails };
