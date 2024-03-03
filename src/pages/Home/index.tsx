636edb5d81cf6718864d304f1fd1540bfe63cbe4
Eliel Gomes Hyertquist <elielgomespg@hotmail.com>
2024-03-02 23:03:23 -0300

    feat: Criado pagina de home

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,8 +1,22 @@
 import React from "react";
+import { Banner, Loading } from "../../components";
+import { useGetMovies } from "../../hooks/use-get-movies";
+import { GridMovieCards } from "../../layout";
+import { GetTopMovieList } from "../../providers/api.provider";
 
-export const Home: React.FC = () => (
-	<main>
-		<h1>Home</h1>
-		<p>Welcome to the home page!</p>
-	</main>
-);
+export const Home: React.FC = () => {
+	const { movies, loading, highlightMovie } = useGetMovies({
+		callback: GetTopMovieList,
+	});
+
+	return loading ? (
+		<Loading />
+	) : (
+		<main className="k-w-full k-pb-24">
+			{highlightMovie && <Banner movie={highlightMovie} bgColorLoad="#000" />}
+			<section className="container k-pt-24">
+				<GridMovieCards movies={movies} />
+			</section>
+		</main>
+	);
+};
