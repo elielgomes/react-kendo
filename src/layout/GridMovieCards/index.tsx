2d557561a23e8aa8ddb6e62426fe7376a4a18a63
Eliel Gomes Hyertquist <elielgomespg@hotmail.com>
2024-03-02 23:00:55 -0300

    Alterado cor dos elementos do grid layout

--- src/layout/GridMovieCards/index.tsx
+++ src/layout/GridMovieCards/index.tsx
@@ -11,7 +11,7 @@ interface IGridMovieCardsProps {
 
 export const GridMovieCards: React.FC<IGridMovieCardsProps> = ({ movies }) => (
 	<GridLayout
-		className="custom-grid-layout k-bg-white k-w-fit k-mx-auto"
+		className="custom-grid-layout k-w-fit k-mx-auto"
 		cols={[{ width: 210 }, { width: 210 }, { width: 210 }, { width: 210 }]}
 		gap={{ rows: 80, cols: 30 }}
 	>
@@ -21,7 +21,7 @@ export const GridMovieCards: React.FC<IGridMovieCardsProps> = ({ movies }) => (
 				className="k-d-flex k-justify-content-center k-align-items-center"
 			>
 				<Link to={`/details/${item.id}`}>
-					<MovieCard movie={item} color="#6f3a8f" />
+					<MovieCard movie={item} color="#1e1e1e" />
 				</Link>
 			</GridLayoutItem>
 		))}
