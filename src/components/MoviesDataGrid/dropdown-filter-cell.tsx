28ff772ad03bbe63cffaa5b88c7b37c23d197168
Eliel Gomes Hyertquist <elielgomespg@hotmail.com>
2024-03-04 08:42:24 -0300

    fix: Ajustado bug no botao de limpar filtro para genero

--- src/components/MoviesDataGrid/dropdown-filter-cell.tsx
+++ src/components/MoviesDataGrid/dropdown-filter-cell.tsx
@@ -28,19 +28,28 @@ export const DropdownFilterCell = (props: DropdownFilterCellProps) => {
 		});
 	};
 	return (
-		<div className="k-filtercell">
+		<div className="k-filtercell k-gap-1">
 			<DropDownList
 				data={props.data}
 				onChange={onChange}
 				value={props.value || props.defaultItem}
 				defaultItem={props.defaultItem}
 			/>
-			<Button
-				title="Clear"
-				disabled={!hasValue(props.value)}
-				onClick={onClearButtonClick}
-				icon="filter-clear"
-			/>
+			<Button title="Clear" disabled={!hasValue(props.value)} onClick={onClearButtonClick}>
+				<span
+					className="k-icon k-svg-icon k-svg-i-filter-clear k-button-icon"
+					aria-hidden="true"
+				>
+					<svg
+						aria-hidden="true"
+						focusable="false"
+						xmlns="http://www.w3.org/2000/svg"
+						viewBox="0 0 512 512"
+					>
+						<path d="m143.5 64 168.2 168.2L288 256v160l-64 64V256L64 96V64zm236.1 100.4L448 96V64H279.3l-64-64L192 22l298 298 22-23.3z" />
+					</svg>
+				</span>
+			</Button>
 		</div>
 	);
 };
