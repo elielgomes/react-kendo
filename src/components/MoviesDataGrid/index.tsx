243bdb7b00e85d96231da4f7b97d2b26ed669edd
Eliel Gomes Hyertquist <elielgomespg@hotmail.com>
2024-03-04 08:44:25 -0300

    feat: Adicionado ordenação para campos de comparacao numerica

--- src/components/MoviesDataGrid/index.tsx
+++ src/components/MoviesDataGrid/index.tsx
@@ -1,27 +1,55 @@
 /* eslint-disable react/jsx-max-props-per-line */
 
-import { CompositeFilterDescriptor, DataResult, filterBy } from "@progress/kendo-data-query";
+import {
+	CompositeFilterDescriptor,
+	DataResult,
+	filterBy,
+	orderBy,
+	SortDescriptor,
+} from "@progress/kendo-data-query";
 import {
 	Grid,
 	GridColumn,
+	GridCustomCellProps,
 	GridFilterCellProps,
 	GridFilterChangeEvent,
 	GridFilterOperators,
 	GridPageChangeEvent,
+	GridSortChangeEvent,
 } from "@progress/kendo-react-grid";
+import { Rating } from "@progress/kendo-react-inputs";
 import React, { useEffect } from "react";
 import { IGenreList } from "../../interfaces";
-import { GetGenreList, GetTopMovieList } from "../../providers/api.provider";
+import { GetGenreList, GetPopularMovieList } from "../../providers/api.provider";
 import { joinGenresWithMovies } from "../../utils/join-genres-movies";
 import { Loading } from "../Loading";
 import { DropdownFilterCell } from "./dropdown-filter-cell";
+import "./style.scss";
 
 interface ICategoryFilterProps extends GridFilterCellProps {
 	data: string[];
 }
 
+interface PageState {
+	skip: number;
+	take: number;
+}
+
+export const RatingCell = (props: GridCustomCellProps) => {
+	const field = "voteAverage" || "";
+	const value = Math.floor(Number(props.dataItem[field]) / 2);
+	if (props.rowType === "groupHeader") {
+		return null;
+	}
+	return (
+		<td {...props.tdProps}>
+			<Rating value={value === null ? undefined : value} readonly />{" "}
+		</td>
+	);
+};
+
 const CategoryFilterCell = (props: ICategoryFilterProps) => (
-	<DropdownFilterCell {...props} data={props.data} defaultItem="Select category" />
+	<DropdownFilterCell {...props} data={props.data} defaultItem="Selecione um gênero" />
 );
 
 const filterOperators: GridFilterOperators = {
@@ -31,10 +59,7 @@ const filterOperators: GridFilterOperators = {
 	boolean: [{ text: "grid.filterEqOperator", operator: "eq" }],
 };
 
-interface PageState {
-	skip: number;
-	take: number;
-}
+const initialSort: SortDescriptor[] = [{ field: "ProductName", dir: "asc" }];
 
 const initialDataState: PageState = { skip: 0, take: 20 };
 
@@ -45,19 +70,20 @@ export const MoviesDataGrid = () => {
 	const [auxData, setAuxData] = React.useState<DataResult>({ data: [], total: 0 });
 	const [filter, setFilter] = React.useState<CompositeFilterDescriptor>();
 	const [page, setPage] = React.useState<PageState>(initialDataState);
+	const [sort, setSort] = React.useState(initialSort);
 
-	const fetchData = async (page: number) => {
+	const fetchData = async (pageNumber: number) => {
 		setLoading(true);
 		try {
 			const genreList = await GetGenreList();
-			const moviesList = await GetTopMovieList(page);
+			const moviesList = await GetPopularMovieList(pageNumber);
 
 			const dataResult: DataResult = {
 				data: joinGenresWithMovies(moviesList, genreList).results.map((movie) => ({
 					id: movie.id,
 					title: movie.title,
 					genres: movie.genres.map((genre) => genre.name).join(", "),
-					releaseDate: new Date(movie.release_date).toLocaleDateString("pt-BR"),
+					releaseDate: new Date(movie.release_date).getFullYear(),
 					voteAverage: movie.vote_average.toFixed(1),
 					genre: movie.genres[0].name || "Sem gênero",
 				})),
@@ -86,9 +112,20 @@ export const MoviesDataGrid = () => {
 	};
 
 	const pageChange = (event: GridPageChangeEvent) => {
-		console.log(event.page.skip / event.page.take + 1);
 		setPage({
 			...event.page,
+		});
+	};
+
+	const sortChange = (event: GridSortChangeEvent) => {
+		setSort(event.sort);
+		if (!event.sort) {
+			setAuxData(movies);
+			return;
+		}
+		setAuxData({
+			data: movies ? orderBy(movies?.data, event.sort) : [],
+			total: movies ? orderBy(movies?.data, event.sort).length : 0,
 		});
 	};
 
@@ -102,6 +139,8 @@ export const MoviesDataGrid = () => {
 		</div>
 	) : (
 		<Grid
+			// eslint-disable-next-line max-len
+			className="k-rounded-lg k-overflow-hidden"
 			style={{ height: "500px" }}
 			data={auxData}
 			filterable
@@ -116,17 +155,25 @@ export const MoviesDataGrid = () => {
 			filterOperators={filterOperators}
 			pageSize={20}
 			onPageChange={pageChange}
+			sortable
+			sort={sort}
+			onSortChange={sortChange}
 		>
 			<GridColumn field="id" title="ID" width="140px" filterable={false} />
 			<GridColumn field="title" title="Nome" width="250px" />
-			<GridColumn field="releaseDate" filter="text" title="Lançamento" />
-			<GridColumn field="voteAverage" filter="text" title="Avaliação" />
 			<GridColumn
 				field="genre"
 				title="Gênero"
 				filterCell={(e) => (
 					<CategoryFilterCell {...e} data={allGenres?.genres.map((g) => g.name) || []} />
 				)}
+			/>
+			<GridColumn field="releaseDate" filterable={false} title="Ano de lançamento" />
+			<GridColumn
+				field="voteAverage"
+				title="Avaliação"
+				filterable={false}
+				cells={{ data: RatingCell }}
 			/>
 		</Grid>
 	);
