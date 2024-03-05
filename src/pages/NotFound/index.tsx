0ad0681c52ca653df327b0f43703bd674aded49a
Eliel Gomes Hyertquist <elielgomespg@hotmail.com>
2024-03-04 22:26:43 -0300

    feat: Criado pagina de 404

--- src/pages/NotFound/index.tsx
+++ src/pages/NotFound/index.tsx
@@ -1,7 +1,20 @@
 import React from "react";
+import notfoundImage from "../../assets/img/404.png";
+import "./style.scss";
 
 export const NotFound: React.FC = () => (
-	<main>
-		<h1>Not Found</h1>
+	<main className="container k-px-8 k-h-screen k-d-flex k-align-items-center k-justify-content-center">
+		<div className="notfound-wrapper k-mt-8">
+			<p className="k-font-size-xl text-primary k-text-center">Ooops...</p>
+			<p className="text k-text-white k-text-center k-mb-2">Página não encontrada!</p>
+			<img src={notfoundImage} alt="Not found" className="notfound-img" />
+			<p className="k-text-white k-font-size-xl k-font-semibold k-text-center">
+				PEGUE SUA PIPOCA E ASSISTA OS
+				<br /> <strong className="text-primary">MELHORES</strong> FILMES
+			</p>
+			<a href="/" className="k-mt-3">
+				Home
+			</a>
+		</div>
 	</main>
 );
